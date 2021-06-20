4e26facdb25535bbaaf5db53082db387ed1654cd
shinyay <shinya.com@gmail.com>
2021-06-21 07:08:09 +0900

    Add an EventListener for User creating event

--- src/main/kotlin/com/google/shinyay/listener/UserEventingListener.kt
+++ src/main/kotlin/com/google/shinyay/listener/UserEventingListener.kt
@@ -1,7 +1,15 @@
 package com.google.shinyay.listener
 
+import com.google.shinyay.event.UserCreationEvent
+import com.google.shinyay.logger
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class UserEventingListener {
+
+    @EventListener(UserCreationEvent::class)
+    fun reportUserCreation(event: UserCreationEvent) {
+        logger.info("New User Created: $event")
+    }
 }
