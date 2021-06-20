08a039a3996ba8abe4ffe56176b4dcbc3e345ae0
shinyay <shinya.com@gmail.com>
2021-06-21 06:07:12 +0900

    Add ApplicationEventPublisher injection

--- src/main/kotlin/com/google/shinyay/service/UserService.kt
+++ src/main/kotlin/com/google/shinyay/service/UserService.kt
@@ -1,7 +1,10 @@
 package com.google.shinyay.service
 
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
+
 @Service
-class UserService {
+class UserService(val eventPublisher: ApplicationEventPublisher) {
+
 }
