66c6aa977278b2164e8259947e00304a93eb141e
shinyay <shinya.com@gmail.com>
2021-06-21 06:11:18 +0900

    Add event publishing to create user

--- src/main/kotlin/com/google/shinyay/service/UserService.kt
+++ src/main/kotlin/com/google/shinyay/service/UserService.kt
@@ -1,10 +1,20 @@
 package com.google.shinyay.service
 
+import com.google.shinyay.event.UserCreationEvent
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
+import java.util.concurrent.ThreadLocalRandom
+
+
+
 
 
 @Service
 class UserService(val eventPublisher: ApplicationEventPublisher) {
 
+    fun createUser(userName: String): Long {
+        val primaryKey = ThreadLocalRandom.current().nextLong(1, 1000)
+        this.eventPublisher.publishEvent(UserCreationEvent(this, userName, primaryKey))
+        return primaryKey
+    }
 }
