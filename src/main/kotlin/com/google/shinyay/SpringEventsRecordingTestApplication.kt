be793f181cff156597498b86799c773cf438173a
shinyay <shinya.com@gmail.com>
2021-06-21 07:03:32 +0900

    Add custom logger

--- src/main/kotlin/com/google/shinyay/SpringEventsRecordingTestApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringEventsRecordingTestApplication.kt
@@ -1,5 +1,7 @@
 package com.google.shinyay
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -9,3 +11,6 @@ class SpringEventsRecordingTestApplication
 fun main(args: Array<String>) {
 	runApplication<SpringEventsRecordingTestApplication>(*args)
 }
+
+val Any.logger: Logger
+	get() = LoggerFactory.getLogger(this.javaClass)
