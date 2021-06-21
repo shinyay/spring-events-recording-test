bc93b5fa6e27525d87c9070c739ed38e7abba49f
shinyay <shinya.com@gmail.com>
2021-06-21 09:02:48 +0900

    Add @RecordApplicationEvents test class

--- src/test/kotlin/com/google/shinyay/UserServiceContextTest.kt
+++ src/test/kotlin/com/google/shinyay/UserServiceContextTest.kt
@@ -2,9 +2,11 @@ package com.google.shinyay
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.event.RecordApplicationEvents
 
 @SpringBootTest
-class SpringEventsRecordingTestApplicationTests {
+@RecordApplicationEvents
+class UserServiceContextTest {
 
 	@Test
 	fun contextLoads() {
