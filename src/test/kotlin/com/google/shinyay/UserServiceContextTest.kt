a55c14327f81a8be600e6d5b1c6de74ff17ea83e
shinyay <shinya.com@gmail.com>
2021-06-21 23:01:31 +0900

    Add test function to check ApplicationEvnts fired or not

--- src/test/kotlin/com/google/shinyay/UserServiceContextTest.kt
+++ src/test/kotlin/com/google/shinyay/UserServiceContextTest.kt
@@ -1,20 +1,36 @@
 package com.google.shinyay
 
+import com.google.shinyay.event.UserCreationEvent
 import com.google.shinyay.service.UserService
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.event.ApplicationEvents
 import org.springframework.test.context.event.RecordApplicationEvents
 
+
 @SpringBootTest
 @RecordApplicationEvents
-class UserServiceContextTest(
-	@Autowired val applicationEvents: ApplicationEvents,
-	@Autowired val userService: UserService) {
+class UserServiceContextTest() {
+	@Autowired
+	private val applicationEvents: ApplicationEvents? = null
+
+	@Autowired
+	private val userService: UserService? = null
 
 	@Test
 	fun createUserShouldPublishEvents() {
+
+
+
+		this.userService?.createUser("Alice")
+
+		assertEquals(1, applicationEvents
+			?.stream(UserCreationEvent::class.java)
+			?.filter { event: UserCreationEvent -> event.username == "Alice" }
+			?.count())
+		applicationEvents?.stream()?.forEach(System.out::println)
 	}
 
 }
