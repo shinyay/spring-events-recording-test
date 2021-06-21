142c2b99e847bbea1b23d9261fbc75fc28b206a5
shinyay <shinya.com@gmail.com>
2021-06-21 23:00:58 +0900

    Add constructor injection of ApplicationEvents to test class

--- src/test/kotlin/com/google/shinyay/UserServiceContextTest.kt
+++ src/test/kotlin/com/google/shinyay/UserServiceContextTest.kt
@@ -1,15 +1,20 @@
 package com.google.shinyay
 
+import com.google.shinyay.service.UserService
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.event.ApplicationEvents
 import org.springframework.test.context.event.RecordApplicationEvents
 
 @SpringBootTest
 @RecordApplicationEvents
-class UserServiceContextTest {
+class UserServiceContextTest(
+	@Autowired val applicationEvents: ApplicationEvents,
+	@Autowired val userService: UserService) {
 
 	@Test
-	fun contextLoads() {
+	fun createUserShouldPublishEvents() {
 	}
 
 }
