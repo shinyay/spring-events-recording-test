452a2752b62375c983625109655ca807b11f2907
shinyay <shinya.com@gmail.com>
2021-06-24 06:19:01 +0900

    Modify return HTTP Status as created

--- src/main/kotlin/com/google/shinyay/controller/UserController.kt
+++ src/main/kotlin/com/google/shinyay/controller/UserController.kt
@@ -1,6 +1,7 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.service.UserService
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,6 +17,6 @@ class UserController(val userService: UserService) {
     @PostMapping("/users")
     fun createNewUser(@RequestBody userName: String): ResponseEntity<Long> {
         val id = userService.createUser(userName)
-        return ResponseEntity.ok(id)
+        return ResponseEntity.status(HttpStatus.CREATED).body(id)
     }
 }
