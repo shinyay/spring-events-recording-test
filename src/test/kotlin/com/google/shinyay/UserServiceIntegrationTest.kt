c8487845f9790e04f2a0d523bbc1c55b554a336e
shinyay <shinya.com@gmail.com>
2021-06-24 06:19:56 +0900

    Add CREATED by POST test

--- src/test/kotlin/com/google/shinyay/UserServiceIntegrationTest.kt
+++ src/test/kotlin/com/google/shinyay/UserServiceIntegrationTest.kt
@@ -1,7 +1,18 @@
 package com.google.shinyay
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.postForEntity
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class UserServiceIntegrationTest {
+class UserServiceIntegrationTest(@Autowired val testRestTemplate: TestRestTemplate) {
+
+    @Test
+    fun createUserShouldReturnCreated() {
+        val result = testRestTemplate.postForEntity("/api/v1/users", "Alice", Long::class.java)
+        assertThat(result.statusCodeValue).isEqualTo(201)
+    }
 }
