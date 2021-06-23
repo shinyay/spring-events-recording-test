a4ff6d7a04bf780b65ef8afff3c0c0a853eea6eb
shinyay <shinya.com@gmail.com>
2021-06-24 06:08:49 +0900

    Add controller class with PostMethod

--- src/main/kotlin/com/google/shinyay/controller/UserController.kt
+++ src/main/kotlin/com/google/shinyay/controller/UserController.kt
@@ -1,6 +1,10 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.service.UserService
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1")
 class UserController(val userService: UserService) {
-    
+
+    @PostMapping("/users")
+    fun createNewUser(@RequestBody userName: String): ResponseEntity<Long> {
+        val id = userService.createUser(userName)
+        return ResponseEntity.ok(id)
+    }
 }
