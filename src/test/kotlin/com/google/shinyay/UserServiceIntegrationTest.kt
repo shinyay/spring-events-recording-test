2b8c3635acccd34c1e509ae414c1ba6552931dad
shinyay <shinya.com@gmail.com>
2021-06-23 22:14:35 +0900

    Add Integration test for UserService

---
+++ src/test/kotlin/com/google/shinyay/UserServiceIntegrationTest.kt
@@ -0,0 +1,7 @@
+package com.google.shinyay
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserServiceIntegrationTest {
+}
